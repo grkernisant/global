8dfd78b11ce73d30e39641a8b59bc7dfbf7a6c59
Guy-Robert Kernisant <grkernisant@gmail.com>
2015-10-27 12:24:51 +0100

    Related Posts

--- js/admin/post.related.js
+++ js/admin/post.related.js
@@ -16,6 +16,26 @@ var CUSTOM_RELATED_POSTS = {
 	onRelatedPostClick: function(event) {
 		var p = jQuery(event.currentTarget);
 		p.toggleClass('selected');
+
+		CUSTOM_RELATED_POSTS.updateRelatedPosts(p.parents('.related-posts'));
+	},
+	updateRelatedPosts: function(post) {
+		var related = new Array();
+		post.children('li.selected').each(function() {
+			related.push(jQuery(this).attr('data-post_id'));
+		});
+
+		// html update
+		post.attr('data-related_posts', related.join(''));
+		// db update
+		var data = {
+			__actions: 'post-meta-update',
+			action: 'data_request',
+			post_id: post.attr('data-post_id'),
+			meta_key: post.attr('data-related_key'),
+			meta_value: related
+		};
+		jQuery.post(ajaxurl, data, 'json');
 	}
 };
 
